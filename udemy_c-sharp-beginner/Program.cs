3dbce16be15538a3d2d1b31cc888f18f7aecff07
nwest88 <nicalaus.west@gmail.com>
2014-01-20 23:51:48 -0600

    Chap6Part3 Incramenting and decramenting varaibles and how to use composites to save time. Reported in https://app.asana.com/0/9404392569325/9404392569327

--- udemy_c-sharp-beginner/Program.cs
+++ udemy_c-sharp-beginner/Program.cs
@@ -9,7 +9,22 @@ namespace udemy_c_sharp_beginner
     {
         static void Main(string[] args)
         {
-            int number = (2 + 3) * 4;
+            int number = 1;
+            number = number + 1;
+            number = number - 1;
+            //prefix form for incramenting a variable
+            ++number;
+            //suffix form for incramenting a variable
+            number++;
+            //prefix form for decramenting a variable
+            ++number;
+            //suffix form for decramenting a variable
+            number++;
+            //to increase a variable by more than one
+            number += 2; //number = number + 2;
+            number -= 3; //number = number - 3;
+            number *= 4;
+            number /= 5;
             Console.WriteLine(number);
             Console.ReadKey();
 
