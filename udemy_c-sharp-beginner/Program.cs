1621204806cf1ad20424f58e174a7f5fc9bdd385
nwest88 <nicalaus.west@gmail.com>
2014-01-20 23:22:21 -0600

    Starting Chap6Part1

--- udemy_c-sharp-beginner/Program.cs
+++ udemy_c-sharp-beginner/Program.cs
@@ -5,71 +5,11 @@ using System.Text;
 
 namespace udemy_c_sharp_beginner
 {
-    class Date
-    {
-        private int month, day, year;
-
-        public Date(int m, int d, int y)
-        {
-            month = m;
-            day = d;
-            year = y;
-        }
-
-        public override string ToString()
-        {
-            return month + "/" + day + "/" + year;
-        }
-
-        public int Month
-        {
-            get
-            {
-                return month;
-            }
-            set
-            {
-                month = value;
-            }
-        }
-        public int Day
-        {
-            get
-            {
-                return day;
-            }
-            set
-            {
-                day = value;
-            }
-        }
-        public int Year
-        {
-            get
-            {
-                return year;
-            }
-            set
-            {
-                year = value;
-            }
-        }
-    }
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            Date today = new Date(7, 12, 2011);
-            Console.WriteLine("Today is " + today.ToString());
-            today.Day = 11;
-            today.Month = 8;
-            Console.WriteLine("Today is " + today.ToString());
-            Console.WriteLine(today.Month + "/" + today.Day +
-                "/" + today.Year);
-            Console.ReadKey();
         }
     }
 }
+
